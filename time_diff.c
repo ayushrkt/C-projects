4c3b70059557ca120f9938214052a2dad22a55c5
ayushrkt <75066755+ayushrkt@users.noreply.github.com>
2020-11-26 19:04:10 +0400

    yes

--- time_diff.c
+++ time_diff.c
@@ -15,14 +15,14 @@ if(scanf("%u : %u : %u , %u", &h2, &m2, &s2, &ms2)!=4 || h2>23 || m2 > 59 || s2 
 }
 
 
-unsigned int absoltime_1 = ms1 + (1000*s1) + (60*1000*m1) + (60*60*1000*h1);
-unsigned int absoltime_2 = ms2 + (1000*s2) + (60*1000*m2) + (60*60*1000*h2);
+unsigned int absoltime_1 = ms1 + (1000*s1) + (60*1000*m1) + (60*60*1000*h1); //converts the entire time to milliseconds
+unsigned int absoltime_2 = ms2 + (1000*s2) + (60*1000*m2) + (60*60*1000*h2); 
 int time_difference= absoltime_2 - absoltime_1; 
-if(time_difference< 0){
+if(time_difference< 0){ //sanitizes input.
 	printf("Invalid input.\n");
 	return 0;
 }
-int h, m, s, ms;
+int h, m, s, ms; //int initialization helps in getting integer solid values for final result
 h= time_difference/(60*60*1000);
 int left1 = time_difference%(60*60*1000);
 m = left1/(60*1000);
@@ -31,6 +31,6 @@ s= left2/1000;
 ms= left2%1000;
 
 
-printf("Duration: %2d:%02d:%02d,%03d\n", h, m, s, ms);
+printf("Duration: %2d:%02d:%02d,%03d\n", h, m, s, ms); //result
 return 0;
 }
